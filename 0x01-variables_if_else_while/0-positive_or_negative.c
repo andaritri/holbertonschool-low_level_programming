9f7c7185d824bb8b4609ee873375e763e03efda5
aarizatr <aarizatr@gmail.com>
2020-06-11 21:21:19 +0000

    Include header for input and output

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - Check if a number.
@@ -16,15 +17,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative", n);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-		printf("%d is zzero", n);
+		printf("%d is zzero\n", n);
 	}
 	return (0);
 }
