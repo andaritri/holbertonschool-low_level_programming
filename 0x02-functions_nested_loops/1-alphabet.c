a66c128f1ae10fb38ab766a46d06b8cb7f0ec86a
aarizatr <aarizatr@gmail.com>
2020-06-16 14:16:20 +0000

    Display alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - Print alphabet in lowercase
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	return;
+}
