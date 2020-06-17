df1c9a5558cd747fd16c601b8f8bd7297a06c20c
aarizatr <aarizatr@gmail.com>
2020-06-17 22:43:10 +0000

    Sum fibo less 4 millions, even

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,24 +2,30 @@
 
 /**
  * main - Entry point
+ *
+ * Return: Always 0
  */
 
-void main(void)
+int main(void)
 {
-	unsigned long first, second, c, next;
+	signed long long first, second, c, next;
 
 	first = 1;
 	second = 2;
 	c = 2;
 
-	printf("%lu, %lu", first, second);
-	while (c  < 50)
+	next = first + second;
+	while (next  < 4000000)
 	{
 		next = first + second;
 		first = second;
 		second = next;
-		printf(", %lu", next);
-		c++;
+		if (next % 2 == 0)
+		{
+			c = c + next;
+		}
 	}
+	printf("%lli", c);
 	printf("\n");
+	return (0);
 }
