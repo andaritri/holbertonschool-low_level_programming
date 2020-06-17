d5617e563f85c2dddf6443ba0602f1e1a712ffa1
aarizatr <aarizatr@gmail.com>
2020-06-17 06:19:52 +0000

    Print numbers until 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - Print numbers from n to 98
@@ -14,39 +15,29 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= stop; i--)
 		{
-			_putchar('0' + (i / 10));
-			_putchar('0' + (i % 10));
 			 if (i != 98)
-			  {
-				  _putchar(',');
-				  _putchar(' ');
-			   }
+			 {
+				  printf("%d, ", i);
+			  }
+			 else
+			 {
+				 printf("%d", i);
+			 }
 		}
 	}
 	else
 	{
 		for (i = n; i <= stop; i++)
 		{
-			if (i / 10 != 0)
+			if (i != 98)
 			{
-				_putchar('0' + (i / 10));
-				_putchar('0' + (i % 10));
-				if (i != 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				printf("%d, ", i);
 			}
 			else
 			{
-				_putchar('0' + (i / 10));
-				if (i != 98)
-				{
-					_putchar(';');
-					_putchar(' ');
-				}
+				printf("%d", i);
 			}
 	        }
 	}
-	_putchar('\n');
+	printf("\n");
 }
