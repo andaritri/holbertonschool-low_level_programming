e91cbae9b2735debaa1d9d3910c52643ff338539
aarizatr <aarizatr@gmail.com>
2020-06-17 05:34:03 +0000

    Delete any unnecessary conditionals

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,19 +24,17 @@ void times_table(void)
 					_putchar(' ');
 				}
 			}
-			else if (n <= 9 && i * (j + 1) >= 10)
-			{
-				_putchar(n + '0');
-				if (c != 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
 			else
 			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
+				if (n / 10 == 0)
+				{
+					_putchar(n + '0');
+				}
+				else
+				{
+					_putchar((n / 10) + '0');
+					_putchar((n % 10) + '0');
+				}
 				if (c != 9)
 				{
 					_putchar(',');
