82826ed376d4894b7c820db4a969c2e01e619f26
aarizatr <aarizatr@gmail.com>
2020-06-17 04:57:43 +0000

    Optimize code

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,53 +6,44 @@
 
 void times_table(void)
 {
-	int i, j, n;
-	int count;
+	int i, j, n, c;
 
 	for (i = 0; i <= 9; i++)
 	{
-		count = 0;
+		c = 0;
 		for (j = 0; j <= 9; j++)
 		{
 			n = i * j;
-			if (n / 10 == 0)
+			if (n <= 9 && i * (j + 1) <= 9)
 			{
-				_putchar((n % 10) + '0');
-				if (count != 9)
+				_putchar(n + '0');
+				if (c != 9)
 				{
-					if (i * (j + 1) > 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-					else
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-					}
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+			else if (n <= 9 && i * (j + 1) >= 10)
+			{
+				_putchar(n + '0');
+				if (c != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
 				}
 			}
 			else
 			{
-				_putchar('0' + (n / 10));
-				_putchar('0' + (n % 10));
-				if (count != 9)
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				if (c != 9)
 				{
-					if (i * (j + 1) > 9)
-                                        {
-						_putchar(',');
-						_putchar(' ');
-					}
-					else
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-					}
+					_putchar(',');
+					_putchar(' ');
 				}
 			}
-			count++;
+			c++;
 		}
 		_putchar('\n');
 	}
