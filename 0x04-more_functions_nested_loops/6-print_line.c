e6aa8ac6f76334fd56f3f391f7f87487fe6f5543
aarizatr <aarizatr@gmail.com>
2020-06-18 22:53:51 +0000

    Print straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_line - Print numbers from 0 to 14
+ * @n: Number times
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		i = 0;
+		while (i < n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
