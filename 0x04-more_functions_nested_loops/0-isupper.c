5d9a6c16797359effd0e31ddb0205cdd1e020a65
aarizatr <aarizatr@gmail.com>
2020-06-18 15:21:51 +0000

    Check character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+
+/**
+ * _isupper - Check caracter
+ * @c: Character for checking
+ *
+ * Return: 1 if the character is uppercase, 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
