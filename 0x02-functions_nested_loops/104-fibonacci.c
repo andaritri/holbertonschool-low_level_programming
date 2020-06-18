7df03fbf6bfd0d97edc4f8f4b45dcea0bca6105c
aarizatr <aarizatr@gmail.com>
2020-06-18 00:33:17 +0000

    Calcute first 98 fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	unsigned long first, second, c, next;
+
+	first = 1;
+	second = 2;
+	c = 2;
+
+	printf("%lu, %lu", first, second);
+	while (c  < 98)
+	{
+		next = first + second;
+		first = second;
+		second = next;
+		printf(", %lu", next);
+		c++;
+	}
+	printf("\n");
+	return (0);
+}
