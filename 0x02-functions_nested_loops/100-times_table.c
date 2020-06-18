838cc1b17b79e2059080a3890612e83dc921075e
aarizatr <aarizatr@gmail.com>
2020-06-18 04:59:56 +0000

    Print table times

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * print_times_table - Print table
+ * @n: Table for printing
+ *
+ * return: Nothing
+ */
+
+void print_times_table(int n)
+{
+	int row, col, r;
+
+	if (n >= 0 && n <= 15)
+	{
+		row = 0;
+		while (row <= n)
+		{
+			col = 0;
+			while (col <= n)
+			{
+				r = row * col;
+				if (col != n)
+				{
+					if (row + r < 10)
+					{
+						printf("%d,   ", r);
+					}
+					else if (row + r >= 10 && row + r < 100)
+					{
+						printf("%d,  ", r);
+					}
+					else if (row + r >= 100)
+					{
+						printf("%d, ", r);
+					}
+				}
+				else
+				{
+					printf("%d", r);
+				}
+				col++;
+			}
+			row++;
+			printf("\n");
+		}
+	}
+}
