474a9b1ab5f36f66101f38980b9ba7d950263bbf
aarizatr <aarizatr@gmail.com>
2020-06-24 04:51:27 +0000

    Copy string inside another variable

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * *_strcpy - copy string
+ * @dest: array
+ * @src: string to copy
+ *
+ * Return: String copied
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int c = 0;
+
+	while (1)
+	{
+		dest[c] = src[c];
+		if (src[c] == '\0')
+			break;
+		c++;
+	}
+	return (dest);
+}
