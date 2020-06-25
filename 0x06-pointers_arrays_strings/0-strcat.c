3530e82fb863a4136aa481bd58d7f6c6a8991470
aarizatr <aarizatr@gmail.com>
2020-06-25 21:35:02 +0000

    Concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+/**
+ * char *_strcat - Joint two string
+ * @dest: String to destine
+ * @src: String source
+ *
+ * Return: Pointer to string concatenated
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*p != '\0')
+		p++;
+	while (1)
+	{
+		*p = *src;
+		if (*src == '\0')
+			break;
+		p++;
+		src++;
+	}
+	return (dest);
+}
