1e794777315f18b321c4d254f2e235b0829047b7
aarizatr <aarizatr@gmail.com>
2020-06-26 00:53:45 +0000

    Compare two string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,39 @@
+/**
+ *_strcmp - Compare two string
+ * @s1: First string
+ * @s2: Second String
+ *
+ * Return: Integer number.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	char *r = s1;
+	char *l = s2;
+	int res;
+
+	while (1)
+	{
+		if (*l != *r)
+		{
+			res = *l - *r;
+			break;
+		}
+		else
+		{
+			if (*(l + 1)  == '\0' && *(r + 1) == '\0')
+			{
+				res = 0;
+				break;
+			}
+
+			else
+			{
+				l++;
+				r++;
+				continue;
+			}
+		}
+	}
+	return (res);
+}
