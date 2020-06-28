fe3db709c594d1b89ad921edcfa53df0aa7fc014
aarizatr <aarizatr@gmail.com>
2020-06-28 05:54:16 +0000

    memset function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+/**
+ * _memset - fill memory with a constant byte.
+ * @s:Pointer to the block of memory to fill.
+ * @b:Value to be set. The value is passed as an int.
+ * @n:Number of bytes to be set to the value.
+ *
+ *Return: Pointer to block memory filled.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int count = 0;
+
+	while (count < n)
+	{
+		*s = b;
+		count++;
+		s++;
+	}
+	return (s);
+}
