867773c732c3bfcba8b94e7f8e8eb9baad4caabf
aarizatr <aarizatr@gmail.com>
2020-06-28 16:45:12 +0000

    strpbrk function

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+/**
+ * _strpbrk - find the first character in the string s that matches any
+ * character specified in accept.
+ * @s:string to be scanned
+ * @accept:string that contain the character to match.
+ *
+ * Return: pointer to the character in s that matches one of the characters
+ * in accept or NULL if no such character is found.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *p;
+
+	while (*s)
+	{
+		p = accept;
+		while (*p)
+		{
+			if (*p == *s)
+				return (s);
+			p++;
+		}
+		s++;
+	}
+	return (s);
+}
