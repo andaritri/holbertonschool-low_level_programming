22e2f09b12133a65865a3aeb69495c9fe1a69b74
aarizatr <aarizatr@gmail.com>
2020-06-29 02:56:03 +0000

    set value to char

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+/**
+ * set_string - set the value of a pointer to a char.
+ * @s: string to change
+ * @to: string to point
+ *
+ * Return: nothing.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
