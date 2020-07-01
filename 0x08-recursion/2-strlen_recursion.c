a45094af4cac3f69ef3f70f3b72bf3ec9e4f54d8
aarizatr <aarizatr@gmail.com>
2020-07-01 23:28:32 +0000

    Lenght recursive

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+/**
+ * _strlen_recursion - Calculate leght of a string.
+ * @s: string to calculate lenght.
+ *
+ * Return: lenght of the string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (_strlen_recursion(s + 1) + 1);
+	else
+		return (0);
+}
