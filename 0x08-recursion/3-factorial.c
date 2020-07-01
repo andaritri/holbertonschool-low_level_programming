8f9bffba435bfd11232891cf2b52587d25ce3192
aarizatr <aarizatr@gmail.com>
2020-07-01 23:54:06 +0000

    Recursive factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+/**
+ * factorial - factorial of a number
+ * @n: integer number
+ *
+ * Return: factorial
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
