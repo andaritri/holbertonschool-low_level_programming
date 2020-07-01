64cdf654042f14cd14a569a802bd87733715caa4
aarizatr <aarizatr@gmail.com>
2020-07-01 19:53:24 +0000

    puts function recursive

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - print a string using recursion.
+ * @s: string for printing
+ *
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	if (*++s)
+		_puts_recursion(s);
+	else
+		_putchar('\n');
+}
