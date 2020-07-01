2de5b792ac21d57daf0d87ac8f40a1d70328a812
aarizatr <aarizatr@gmail.com>
2020-07-01 20:34:01 +0000

    Move putchar fuction

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,12 +9,12 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	if (*++s)
-		_puts_recursion(s);
-	else
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
 	}
+	else
+		_putchar('\n');
 }
