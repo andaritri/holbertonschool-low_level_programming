e20445b41596dd7c405d3ed94f5cf1a3e0a079a7
aarizatr <aarizatr@gmail.com>
2020-07-03 01:36:01 +0000

    Change func order

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,21 +1,6 @@
 #include "holberton.h"
 
 /**
- * is_palindrome - Determine if a word is palindrome
- * @s: word to check
- *
- * Return: 1 if the word is palindrome, 0 otherwise.
- *
- */
-
-int is_palindrome(char *s)
-{
-	int len = _strlen_recursion(s);
-
-	return (aux_palindrome(s, 0, len - 1));
-}
-
-/**
  * aux_palindrome - Performe recursive process to determine if a word is
  * palindrome.
  * @s: string to check if it's palindrome
@@ -32,4 +17,19 @@ int aux_palindrome(char *s, int start, int end)
 	else if (*(s + start) != *(s + end))
 		return (0);
 	return (aux_palindrome(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - Determine if a word is palindrome
+ * @s: word to check
+ *
+ * Return: 1 if the word is palindrome, 0 otherwise.
+ *
+ */
+
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (aux_palindrome(s, 0, len - 1));
 }
