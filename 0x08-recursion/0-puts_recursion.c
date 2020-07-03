c59eb98309e0ad4b5fbf04d9f085efbe0a0baf66
aarizatr <aarizatr@gmail.com>
2020-07-03 03:25:12 +0000

    Change structure

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,13 @@
  *
  * Return: nothing
  */
-
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(++s);
 	}
-	_putchar(s + 1);
+	else
+		_putchar('\n');
 }
