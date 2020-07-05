5aa7f98f1134e7f150defc87bd2267258fab008c
aarizatr <aarizatr@gmail.com>
2020-07-05 03:00:27 +0000

    change conditions

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	int n_coins = 0, i = 0;
 	int coins[] = {25, 10, 5, 2, 1};
 
-	if (argc > 2 || argc == 1)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
