74b9ed392be2d37e19b3f2de24e205d6cd51e12e
aarizatr <aarizatr@gmail.com>
2020-07-05 02:57:08 +0000

    change money

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments for the function
+ * @argv: arguments for the function
+ *
+ * Return: always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int cent = 0;
+	int n_coins = 0, i = 0;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc > 2 || argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (atoi(argv[1]) < 0)
+		printf("0\n");
+	cent = atoi(argv[1]);
+	for (i = 0; i < 5; i++)
+	{
+		if (cent % coins[i] == 0)
+		{
+			n_coins += cent / coins[i];
+			break;
+		}
+		else
+		{
+			n_coins += cent / coins[i];
+			cent = cent % coins[i];
+		}
+	}
+	printf("%d\n", n_coins);
+	return (0);
+}
