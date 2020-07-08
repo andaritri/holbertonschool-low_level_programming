9c8359004e79d1a69bb17ffa41a1d0551a49b418
aarizatr <aarizatr@gmail.com>
2020-07-08 01:28:17 +0000

    Allocate memory to 2D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid - pointer to a 2D array.
+ * @width: number of columns
+ * @height: number if rows
+ *
+ * Return: pointer to a 2D array of integers.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, k;
+	int **matrix;
+
+	if ((width == 0) || (height == 0))
+		return (NULL);
+	matrix = malloc(sizeof(int) * height);
+	if (matrix == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+		matrix[i] = malloc(sizeof(int) * width);
+	for (j = 0; j < height; j++)
+	{
+		for (k = 0; k < width; k++)
+			matrix[j][k] = 0;
+	}
+	return (matrix);
+}
