8731d7daf885cbb1711a81b8ab1a80096ff40d1e
aarizatr <aarizatr@gmail.com>
2020-07-09 21:43:03 +0000

    Concat two string

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * string_nconcat - Concatenate two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes to copy from s2 into s1.
+ *
+ * Return: pointer to new string.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	int i;
+	unsigned int j, l = _strlen(s2);
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (n >= (l + 1))
+		n = l;
+	str = malloc(sizeof(char) * (_strlen(s1) + n + 1));
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; s1[i]; i++)
+		str[i] = s1[i];
+	for (j = 0; j <= n; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
+	return (str);
+}
+
+
+/**
+ * _strlen - Calculate the lenght of a string
+ * @s: string
+ *
+ * Return: lenght of the s string
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		c++;
+	}
+	return (c);
+}
