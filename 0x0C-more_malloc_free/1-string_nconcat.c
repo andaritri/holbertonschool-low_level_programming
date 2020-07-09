3d30c3dd19f5b136335eded4beeb4995483ef3b7
aarizatr <aarizatr@gmail.com>
2020-07-09 22:14:03 +0000

    Fix 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	str = malloc(sizeof(char) * (_strlen(s1) + n + 1));
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; s1[i]; i++)
+	for (i = 0; i < _strlen(s1); i++)
 		str[i] = s1[i];
 	for (j = 0; j < n; j++)
 		str[i + j] = s2[j];
