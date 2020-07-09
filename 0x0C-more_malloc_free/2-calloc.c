2d1893245e78a5b0f4a411e72b599919de4d35f1
aarizatr <aarizatr@gmail.com>
2020-07-09 19:54:05 +0000

    Add memset function

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,3 +21,26 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	_memset(p, 0, nmemb * size);
 	return (p);
 }
+
+/**
+* _memset - fill memory with a constant byte.
+* @s:Pointer to the block of memory to fill.
+* @b:Value to be set. The value is passed as an int.
+* @n:Number of bytes to be set to the value.
+*
+* Return: Pointer to block memory filled.
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int count = 0;
+	char *p = s;
+
+	while (count < n)
+	{
+		*p = b;
+		count++;
+		p++;
+	}
+	return (s);
+}
