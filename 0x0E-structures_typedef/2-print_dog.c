826be5b88b1fa838b48c82a35dd5b6f1ae1b89be
aarizatr <aarizatr@gmail.com>
2020-07-13 21:20:15 +0000

    Pint dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print a dog struct.
+ * @d: dog struct
+ *
+ * Return: nothing.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf("\n");
+	if ((*d).name == '\0')
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", (*d).name);
+	printf("Age: %f\n", (*d).age);
+	if ((*d).owner == '\0')
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", (*d).owner);
+}
