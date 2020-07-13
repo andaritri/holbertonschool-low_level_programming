0054c52fbfcca6640064735ab15460ae949ae6ca
aarizatr <aarizatr@gmail.com>
2020-07-13 20:32:39 +0000

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef HEADER_H
+#define HEADER_H
+/**
+ * struct dog - create dog stuct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+#endif
