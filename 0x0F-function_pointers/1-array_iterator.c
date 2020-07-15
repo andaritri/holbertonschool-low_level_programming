e9331d24aa25c92110d54cdc6a944aced3971a49
aarizatr <aarizatr@gmail.com>
2020-07-15 20:35:47 +0000

    check variables

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,6 +13,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+
+	if (action == '\0' || size <= 0 || array == '\0')
+		return;
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
