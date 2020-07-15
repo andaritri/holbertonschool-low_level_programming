a970633050cdf6817135e32d3b33457706399d68
aarizatr <aarizatr@gmail.com>
2020-07-15 14:40:06 +0000

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name - Display name
+ * @name: name of a person.
+ * @f: function pointer
+ *
+ * Return: nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
