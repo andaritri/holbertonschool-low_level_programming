ce234ae97a4eb97ffad8ad0d80ce624672180f74
aarizatr <aarizatr@gmail.com>
2020-07-16 03:10:35 +0000

    Advanced

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Return: always 0.
+ */
+
+int main(int argc, char *argv)
+{
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+}
