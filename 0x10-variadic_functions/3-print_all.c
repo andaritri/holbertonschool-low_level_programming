e2bf9382f2f959062901a5111b56269336082ef0
aarizatr <aarizatr@gmail.com>
2020-07-18 20:41:55 +0000

    function to print anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,97 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_char - print a character.
+ * @c: character for printing.
+ *
+ * Return: nothing.
+ */
+
+void print_char(void *c)
+{
+	printf("%c", *((char *)c));
+}
+
+/**
+ * print_int - print a integer
+ * @i: character for printing.
+ *
+ * Return: nothing.
+ */
+
+void print_int(void *i)
+{
+	printf("%d", *((int *)i));
+}
+
+/**
+ * print_float - print a float number.
+ * @f: character for printing.
+ *
+ * Return: nothing.
+ */
+void print_float(void *f)
+{
+	printf("%f", *((float *)f));
+}
+
+/**
+ * print_str - print a string.
+ * @s: character for printing.
+ *
+ * Return: nothing.
+ */
+
+void print_str(void *s)
+{
+	if (!((char *)s))
+		printf("(nil)");
+	else
+		printf("%s", (char *)s);
+}
+
+/**
+ * print_all - print all the argument passed to func.
+ * @format: format for printing.
+ *
+ * Return: nothing.
+ */
+
+void print_all(const char * const format, ...)
+{
+	ft types[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_str},
+		{'\0', NULL}
+	};
+
+	int i = 0, j = 0;
+	void *p;
+	char *separator;
+	va_list list;
+
+	va_start(list, format);
+	separator = "";
+	while (format[i] != '\0' && format != NULL)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == types[j].ch)
+			{
+				printf("%s", separator);
+				p = va_arg(list, void *);
+				types[j].func(p);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(list);
+	printf("\n");
+}
