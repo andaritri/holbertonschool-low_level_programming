1c1a7679f4b1428859997a5225262b1bc21b2c9a
aarizatr <aarizatr@gmail.com>
2020-07-18 18:33:51 +0000

    Set condition to n

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,15 +14,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 
-	if (n == 0)
-		return;
-	va_start(ap, n);
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		printf("%d", va_arg(ap, int));
-		if (i < (n - 1) && separator != NULL)
-			printf("%s", separator);
+		va_start(ap, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(ap, int));
+			if (i < (n - 1) && separator != NULL)
+				printf("%s", separator);
+		}
+		va_end(ap);
+		printf("\n");
 	}
-	va_end(ap);
-	printf("\n");
 }
