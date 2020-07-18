c619cd6448bcb16b58da0d85e8cc73459d6fbd0f
aarizatr <aarizatr@gmail.com>
2020-07-18 20:40:58 +0000

    Add functions for printing format and strcut

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,5 +4,20 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+/**
+ * struct formats - Define format to print.
+ * @ch: pointer to character.
+ * @func: fucn pointer.
+ *
+ * Description: struct to define specifier format.
+ */
+
+typedef struct formats
+{
+	char ch;
+	void (*func)(void *);
+} ft;
+
 void print_all(const char * const format, ...);
 #endif
