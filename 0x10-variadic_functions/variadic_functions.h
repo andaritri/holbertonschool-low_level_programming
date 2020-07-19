5539298b6bf5c3545b1b6e1ba886d5b34c492fa1
aarizatr <aarizatr@gmail.com>
2020-07-19 03:26:50 +0000

    Add stdarg library

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef _HEADER_H_
 #define _HEADER_H_
+#include <stdarg.h>
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
@@ -16,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...);
 typedef struct formats
 {
 	char ch;
-	void (*func)(void *);
+	void (*func)(va_list);
 } ft;
 
 void print_all(const char * const format, ...);
