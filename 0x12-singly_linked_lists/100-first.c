d56a04897e4a85aa3ef55416d3ee90f2bbbc48fb
aarizatr <aarizatr@gmail.com>
2020-07-24 01:01:02 +0000

    Execute function before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * before_main - print a message before the main function is execute.
+ *
+ * Return: nothing.
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
