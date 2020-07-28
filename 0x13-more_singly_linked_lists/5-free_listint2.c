b14648c0d9e437a32fa9c4d81546da544c1807ea
aarizatr <aarizatr@gmail.com>
2020-07-28 06:10:33 +0000

    Free list linked list and set head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - Add new node at end of a liked list.
+ * @head: head of the linked list.
+ *
+ * Return: nothing.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tp = *head;
+
+	while (tp != NULL)
+	{
+		free(tp);
+		tp = tp->next;
+	}
+	*head = NULL;
+}
