7adb35791b23eb81bd086fbbc8216d66bbba6fd1
aarizatr <aarizatr@gmail.com>
2020-07-30 22:03:47 +0000

    print using _putchar function

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * print_binary - prints the binary representation of a number
@@ -12,5 +11,5 @@ void print_binary(unsigned long int n)
 {
 	if (n > 1)
 		print_binary(n >> 1);
-	printf("%lu", n & 1);
+	_putchar('0' + (n & 1));
 }
