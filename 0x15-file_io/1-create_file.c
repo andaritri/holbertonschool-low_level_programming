b801c0f1bd548d3c607d9cd6e006823c91d3a96c
aarizatr <aarizatr@gmail.com>
2020-08-05 01:16:48 +0000

    Fix bug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,8 +6,6 @@
  *
  * Return: 1 if the file was created, -1 otherwise.
  */
-int _strlen(char *str);
-
 int create_file(const char *filename, char *text_content)
 {
 	int fd_open, fd_write;
@@ -37,7 +35,7 @@ int _strlen(char *str)
 {
 	int count = 0;
 
-	while(*str++)
+	while (*str++)
 		count++;
 	return (count);
 }
