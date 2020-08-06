a9ad0dcd72e74102cfc58b0c9a09738cb0ea390d
aarizatr <aarizatr@gmail.com>
2020-08-06 01:32:06 +0000

    fix bug 4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	inputFD = open(argv[1], O_RDONLY);
