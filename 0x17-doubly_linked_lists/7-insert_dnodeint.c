58a485412795626e996d74c773789a5a8a9062a4
aarizatr <aarizatr@gmail.com>
2020-09-05 18:58:05 +0000

    Change while lopp to for loop

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	tmp = *h;
-	while (tmp != NULL)
+	for (; tmp != NULL; index++, tmp = tmp->next)
 	{
 		if (index == idx)
 		{
@@ -33,8 +33,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			tmp->prev = new;
 			return (new);
 		}
-		index++;
-		tmp = tmp->next;
 	}
 	if (index == idx)
 	{
