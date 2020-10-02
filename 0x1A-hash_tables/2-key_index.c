18625e02bf51b847afd3b2197eb2a4a2299af71b
aarizatr <aarizatr@gmail.com>
2020-10-02 16:01:32 +0000

    Fix betty style

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,6 +4,9 @@
  * key_index - Get the index of a key.
  * @key: Key
  * @size: Size of the array of the hash table.
+ *
+ * Return:  the index at which the key/value pair should be stored in the array
+ * of the hash table.
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
