acf7293d092522aaa5005597b08ea9b2284abe1f
aarizat <aarizatr@gmail.com>
2021-01-14 16:29:56 +0000

    Add prototype function

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,6 +8,7 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int print_array(int *array, size_t left, size_t right);
+int interpolation_search(int *array, size_t size, int value);
 int min(size_t a, size_t b);
 
 #endif
