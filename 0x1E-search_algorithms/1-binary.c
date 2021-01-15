7e7ec831005a41514898b78cdf71e57ef86aaed5
aarizat <aarizatr@gmail.com>
2021-01-15 19:56:27 +0000

    Change variable names

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,26 +1,5 @@
 #include "search_algos.h"
 /**
- * print_array - print the elements of array between the indixes left and right
- * @array: pointer to the first element of the array to print
- * @left: Starting index
- * @right: Ending index
- *
- * Return: always 0.
- */
-
-int print_array(int *array, size_t left, size_t right)
-{
-	printf("Searching in array: ");
-	for (; left <= right; left++)
-	{
-		printf("%i", array[left]);
-		if (array[left] != array[right])
-			printf(", ");
-	}
-	printf("\n");
-	return (0);
-}
-/**
  * binary_search - searches for a value in a sorted array of integers using
  * the Binary search algorithm
  * @array:  pointer to the first element of the array to search in
@@ -47,7 +26,27 @@ int binary_search(int *array, size_t size, int value)
 			right = mid - 1;
 		else
 			left = mid + 1;
-
 	}
 	return (-1);
+}
+/** Function to print an array of integers */
+/**
+ * print_array - print the elements of array between the indixes left and right
+ * @array: pointer to the first element of the array to print
+ * @lo: Starting index
+ * @hi: Ending index
+ *
+ * Return: always 0.
+ */
+int print_array(int *array, size_t lo, size_t hi)
+{
+	printf("Searching in array: ");
+	for (; lo <= hi; lo++)
+	{
+		printf("%i", array[lo]);
+		if (lo != hi)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
 }
