74d566d4a884f9d67863fafc6be53b516e5fb5b8
aarizat <aarizatr@gmail.com>
2021-01-15 21:04:15 +0000

    Add prototype function - Exponential search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,5 +11,7 @@ int print_array(int *array, size_t lo, size_t hi);
 int interpolation_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int binary_aux(int *arr, size_t lo, size_t hi, int val);
+int exponential_search(int *array, size_t size, int value);
+int binary_search_exp(int *array, size_t lo, size_t hi, int value);
 int min(size_t a, size_t b);
 #endif
